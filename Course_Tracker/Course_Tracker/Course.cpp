4410ad18921b80e150fff1ccc54748a19560b40d
Robert-F-Cameron <robert.f.cameron@gmail.com>
2021-04-18 21:49:12 -0700

    debugged course iteration issue. Can now write course and it's categories to a file

--- Course_Tracker/Course_Tracker/Course.cpp
+++ Course_Tracker/Course_Tracker/Course.cpp
@@ -2,41 +2,22 @@
 #include "Course.h"
 
 using namespace std;
-
-//Default Constructor
+//Constructors
 Course::Course() {
 	courseName = "Default Course";
 	courseGrade = 0;
-}
-//Constructor
+};
 Course::Course(string name) {
 	courseName = name;
-}
+};
 //Setters
-void Course::setCourseName(string name) {
-	courseName = name;
-}
-void Course::setCourseGrade() {
-	double grade{};
-	for (unsigned int i{}; i < categories.size(); i++) {
-			grade += categories[i].getCategoryGrade();
-		};
-		courseGrade = grade;
-}
-void Course::setTotalCategoryWeight() {
-	double categoryWeight{};
-	for (unsigned int i{}; i < categories.size(); i++) {
-		categoryWeight += categories[i].getCategoryWeight();
-	}
-	totalCategoryWeight = categoryWeight;
-}
 void Course::addCategory() {
 	//variables
 	string name;
 	double categoryWeight;
 	int numAssignments;
 	//logic
-	if(totalCategoryWeight < 1){
+	if (totalCategoryWeight < 1) {
 		cout << "Category Name: ";
 		getline(cin, name);
 		cout << "Category Grading Weight: ";
@@ -45,14 +26,14 @@ void Course::addCategory() {
 			do {
 				cout << "Invalid Category Grading Weight" << endl;
 				if (totalCategoryWeight > 0) {
-				cout << "Only " << totalCategoryWeight * 100 << "% remaining!" << endl;
+					cout << "Only " << (100 - (totalCategoryWeight * 100)) << "% remaining!" << endl;
 				}
 				else {
-				cout << "Grading weight cannot be greater than 100%" << endl;
+					cout << "Grading weight cannot be greater than 100%" << endl;
 				}
 				cout << "Category Grading Weight: ";
 				cin >> categoryWeight;
-			} while (((categoryWeight * 0.01) + totalCategoryWeight)>1);
+			} while (((categoryWeight * 0.01) + totalCategoryWeight) > 1);
 		}
 		cin.clear();
 		cin.ignore(1024, '\n');
@@ -60,32 +41,48 @@ void Course::addCategory() {
 		cin >> numAssignments;
 		cin.clear();
 		cin.ignore(1024, '\n');
-		categories.push_back(Category(name, categoryWeight, numAssignments));
-		this->setTotalCategoryWeight();
+		Category newCategory = Category(name, categoryWeight, numAssignments);
+		categories.push_back(newCategory);
+		setTotalCategoryWeight();
 		if (totalCategoryWeight < 1) {
 			cout << categories[categories.size() - 1].getCategoryName() << " added!" << ((1 - totalCategoryWeight) * 100) << "% grade weight remaining." << endl;
 		}
 		else if (totalCategoryWeight == 1) {
 			cout << name << " added!" << "100% grade weight allocated." << endl;
 		}
-		cout << endl;
+		cout << "--" << endl;
 	}
-	cout << "Categories Size: " << categories.size();
-}
+};
+void Course::setCourseName(string name) {
+	courseName = name;
+};
+void Course::setCourseGrade() {
+	double grade{};
+	for (unsigned int i{}; i < categories.size(); i++) {
+		grade += categories[i].getCategoryGrade();
+	};
+	courseGrade = grade;
+};
+void Course::setTotalCategoryWeight() {
+	double categoryWeight{};
+	for (unsigned int i{}; i < categories.size(); i++) {
+		categoryWeight += categories[i].getCategoryWeight();
+	}
+	totalCategoryWeight = categoryWeight;
+};
 //Getters
 string Course::getCourseName() {
 	return courseName;
-}
+};
 double Course::getCourseGrade() {
 	return courseGrade;
-}
+};
 double Course::getTotalCategoryWeight() {
 	return totalCategoryWeight;
-}
+};
 int Course::getCategoriesSize() {
 	return categories.size();
-}
-vector<Category> Course::getCategoriesVector(){
-	vector<Category> courseCategories = categories;
-	return courseCategories;
-}
+};
+vector<Category> Course::getCategoriesVector() {
+	return categories;
+};
--- Course_Tracker/Course_Tracker/main.cpp
+++ Course_Tracker/Course_Tracker/main.cpp
@@ -1,105 +1,72 @@
 #include <iostream>
 #include <fstream>
-#include <typeinfo>
+#include <vector>
 #include "Course.h"
 #include "Category.h"
 
 using namespace std;
 
 void createCourse();
-void viewCourses();
+void viewCourse();
 void writeToFile();
-void mainMenu();
-
 vector<Course> courses;
 Course selectedCourse;
 
 int main() {
-	cout << "Welcome to your Course Tracker" << endl << endl;
-	mainMenu();
-}
-
-void mainMenu() {
 	int input;
-	cout << "1) Enter a new Course" << endl
-		<< "2) View Course List" << endl
-		<< "3) Save Course Data" << endl
-		<< "4) Exit Application" << endl
-		<< "Selection: ";
-	cin >> input;
-	cin.clear();
-	cin.ignore(1024, '\n');
-	switch (input) {
-	case 1:
-		createCourse();
-		break;
-	case 2:
-		viewCourses();
-		break;
-	case 3:
-		writeToFile();
-		break;
-	case 4:
-		cout << "Goodbye!" << endl;
-		system("pause");
-	default:
-		cout << "Not a Valid Input";
-	}
+	cout << "Welcome to your Course Tracker" << endl;
+	do {
+		cout << endl << "Main Menu" << endl
+			<< "1) Add Course" << endl
+			<< "2) View Course" << endl
+			<< "3) Write to file" << endl
+			<< "Selection: ";
+		cin >> input;
+		cin.clear();
+		cin.ignore(1024, '\n');
+		cout << endl;
+		switch (input) {
+		case 1:
+			createCourse();
+			break;
+		case 2:
+			viewCourse();
+			break;
+		case 3:
+			writeToFile();
+			break;
+		default:
+			cout << "Not a valid input!" << endl;
+		}
+	} while (input != 4);
 }
 
 void createCourse() {
-	string name; 
-	char input{};
-	bool quit = false;
+	string name;
+	double courseWeight;
 	cout << "Course name: ";
 	getline(cin, name);
-	courses.push_back(Course(name));
-	selectedCourse = courses[courses.size() - 1];
-	cout << selectedCourse.getCourseName() << " added!" << endl << endl;
-	if (selectedCourse.getTotalCategoryWeight() < 100) {
-		do {
-			if (selectedCourse.getTotalCategoryWeight() < 1) {
-				cout << "Would you like to add grading categories to " << selectedCourse.getCourseName() << "? (Y/N): ";
-				cin >> input;
-				input = tolower(input);
-				cin.clear();
-				cin.ignore(1024, '\n');
-			}
-			else {
-				quit = true;
-			}
-			switch (input) {
-			case 'y':
-				selectedCourse.addCategory();
-				selectedCourse.getCategoriesSize();
-				break;
-			case 'n':
-				quit = true;
-				cout << endl;
-				break;
-			default:
-				cout << "Not a Valid Input" << endl;
-			}
-		} while (!quit);
-	}
-	mainMenu();
-}
+	selectedCourse = Course(name);
+	do {
+		cout << "Add grading category." << endl;
+		selectedCourse.addCategory();
+		courseWeight = selectedCourse.getTotalCategoryWeight();
+	} while (courseWeight < 1);
+	courses.push_back(selectedCourse);
+	cout << selectedCourse.getCourseName() << " added!" << endl;
+	return;
+};
 
-void viewCourses() {
-	int input{};
-	cout << "Select course to view:" << endl;
+void viewCourse() {
 	for (unsigned int i{}; i < courses.size(); i++) {
-		cout << (i + 1) << ") " << courses[i].getCourseName()<<"\t";
-	}
-	cout << endl << "Selection: ";
-	cin >> input;
-	input = input - 1;
-	if (input < 0 || input > courses.size()) {
-		cout << "Not a valid selection! " << typeid(input).name() << " - " << input << " - " << courses.size();
-	}
-	selectedCourse = courses[input];
-	cout << selectedCourse.getCourseName();
-}
+		cout << courses[i].getCourseName() << endl;
+		selectedCourse = courses[i];
+		vector<Category> courseCategories = selectedCourse.getCategoriesVector();
+		for (unsigned int i{}; i < courseCategories.size(); i++) {
+			cout << courseCategories[i].getCategoryName() << endl;
+		}
+	};
+};
 
 void writeToFile() {
 	ofstream course_data;
@@ -109,16 +76,13 @@ void writeToFile() {
 			vector<Category> categories = courses[i].getCategoriesVector();
 			course_data << "course,";
 			course_data << courses[i].getCourseName() << endl;
-			cout << categories.size();
-			cout << courses[i].getCategoriesVector().size();
-			for (unsigned int i{}; i < categories.size(); i++){
+			for (unsigned int i{}; i < categories.size(); i++) {
 				cout << "In second for loop";
 				course_data << "category,";
 				course_data << categories[i].getCategoryName() << ",";
 				course_data << categories[i].getCategoryWeight() << ",";
 				course_data << categories[i].getNumAssignments() << endl;
-			}
-
-		}
-	}
-}
+			};
+		};
+	};
+};
